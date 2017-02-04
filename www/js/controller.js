300cb6659841f6131b1e23393b9350b0a3448cf1
gagui <gabrieldsguilherme@gmail.com>
2017-02-04 18:11:20 -0200

    Alterando informação sobre o diagrama

--- www/js/controller.js
+++ www/js/controller.js
@@ -2,7 +2,7 @@ var linus = angular.module('linus', []);
 
 linus.controller('CalculoController', ['$scope', function($scope) {
 
-	$scope.informacao = 'A tabela de linus pauling aceita somente um número de elétrons entre 1 e 112! :)'
+	$scope.informacao = 'O diagrama de Linus Pauling aceita somente um número de elétrons entre 1 e 112! :)'
 	$scope.ordemEnergetica = ' ';
 	$scope.ordemGeometrica = ' ';
 	$scope.eletronsPorCamada = ' ';
